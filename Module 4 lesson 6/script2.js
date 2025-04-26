fc8eef782a6b58a8878232fb3f217d7501494189
Tisha0513 <tishavarshney13@gmail.com>
2025-04-26 13:01:35 +0600

    acp

--- Module 4 lesson 6/script2.js
+++ Module 4 lesson 6/script2.js
@@ -1,4 +1,26 @@
-function Piano() {
-    document.getElementById("result").innerHTML = document.getElementById("demo").firstChild.nodeValue;
-    document.getElementById("result1").innerHTML = document.getElementById("demo").childNodes[0].nodeValue;
+
+function me() {
+    var me = ["Likes swimming", "Plays the piano", "Does art", "Likes to run"];
+    document.getElementById("me").innerHTML = me;
+}
+
+function info() {
+    var aboutmyself = {
+        firstName: "Tisha",
+        secondName: "Varshney",
+        work: "Student",
+        age: "13",
+        city: "Dhaka",
+        country: "Bangladeah",
+    };
+    document.getElementById("results").innerHTML = aboutmyself.firstName + aboutmyself.secondName + " is " + aboutmyself.age + " years old. ";
+}
+
+function animals() {
+    var pets = [
+        "dogs", "cats", "bunnies", "panda"
+    ];
+    document.getElementById("animals").innerHTML = animals[2];
+    document.getElementById("me").innerHTML = animals[4];
+    document.getElementById("info").innerHTML = animals[1];
 }
